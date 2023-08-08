3c0e76790e44854866768d9d30f446a5afe5f621
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-08-08 14:38:30 +0100

    Components Created and applied the Header and Content Components But yet to style them according to the demo design

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import './Body.css'
+import React from "react";
+import "./Content.css";
+import Image from "./illustration-editor-mobile.svg";
+import Editord from "./illustration-editor-desktop.svg";
 
 export default function Body() {
   return (
-    <div className='body'>
-        <h5>Designed for the future</h5>
+    <div className="body">
+      <p className="titles">Designed for the future</p>
+      <div className="img-mobile">
+        <img className="em" src={Image} alt="Editor. Mobile version" />
+      </div>
+      <div className="row">
+        <div className="col">
+          <p className="titles">
+          Introducing an <br /> extensible editor
+          </p>
+          <p>
+            Blogr features an exceedingly intuitive interface which lets you focus
+            on one thing: creating content. The editor supports management of
+            multiple blogs and allows easy manipulation of embeds such as images,
+            videos, and Markdown. Extensibility with plugins and themes provide easy
+            ways to add functionality or change the looks of a blog.
+          </p>
+          <p className="titles">
+            Robust content <br /> management
+          </p>
+          <p>
+            Flexible content management enables users to easily move through posts.
+            Increase the usability of your blog by adding customized categories,
+            sections, format, or flow. With this functionality, you’re in full
+            control.
+          </p>
+        </div>
+        <div className="col ed">
+          <img src={Editord} alt="Editor desktop version" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
